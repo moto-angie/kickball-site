7e50190024e96d58d0314c46347e784ae9268aeb
Angie Smith <angies@mammothhr.com>
2017-09-17 17:07:19 -0700

    Pages set up and working

--- app.rb
+++ app.rb
@@ -2,9 +2,28 @@ require "sinatra"
 require_relative "models/player"
 require_relative "models/team"
 require_relative "models/team_data"
+require 'pry'
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 
 get "/" do
-  "<h1>The LACKP Homepage<h1>"
+  '<h1>The LACKP Homepage<h1>
+  <a href="/teams">Teams</a>
+  <br/>
+  <a href="/positions">Positions</a>'
+end
+
+get '/teams' do
+   @teams = Team.all
+   erb :teams_index
+end
+
+get "/teams/:team_name" do
+  @team_name = params[:team_name]
+  @teams = Team.all
+  erb :team_site
+end
+
+get '/positions' do
+  erb :positions_index
 end
--- models/team.rb
+++ models/team.rb
@@ -1,5 +1,10 @@
 require_relative "./team_data"
 
 class Team
-
+  def initialize
+  end
+  
+  def self.all
+    TeamData::ROLL_CALL
+  end
 end
